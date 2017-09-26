ffde6d9c4c0b37c249d3c8b55a0bccaca33559a2
dylan <dylanmbryan@gmail.com>
2017-09-26 11:34:07 -0400

    added time to getCoinPrice

--- gdaxAPI.py
+++ gdaxAPI.py
@@ -1,4 +1,5 @@
 import gdax
+from datetime import datetime
 # gdax library https://github.com/danpaquin/gdax-python/
 
 client = None
@@ -23,21 +24,35 @@ def invalidateClient():
 def getCoinPrice(coin):
 	"""Returns order book in format 
 	"bids": [price, size, number],
-	"asks": [price, size, number]
+	"asks": [price, size, number],
+	"time": time
 
 	where bids is price of buyable coins and
 	asks is price of sellable coins
 
 	e.g. bids is lower price
 	"""
-    return client.get_product_order_book(coin+'-USD', level=1)
+	retunVal = client.get_product_order_book(coin+'-USD', level=1)
+	retunVal['time'] = datetime.now()
+    return retunVal
 
 
 def buyCoin(coin, usd, coinAmount):
+	"""
+	Places a buy order given the coin name, USD amount to to spend, 
+	and the coinMmount to buy
+
+	Returns the orderID for the order placed; used to check status of order
+	"""
 	response = client.buy(price=usd, size=str(coinAmount), product_id=coin+'-USD')
 	orderId = response['id']
+	return orderId
 
 
 def checkOrderStatus(orderId):
+	"""
+	Checks the status of an order given the ID
+	"""
+	# TODO: Consider using this for an order history sqlite table
 	response = client.get_order(orderId)
 	return response['status']
